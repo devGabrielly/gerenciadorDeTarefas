2fdd675348c78629821a6b5db5fcf4b48353d2a3
Annanda <165848254+AnnandaV@users.noreply.github.com>
2025-11-03 07:11:45 -0300

    criação das funções para registro e login do usuário

---
+++ client/src/services/authService.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API_URL = "http://locashost:5000/api/auth/"; //URL base da API
+
+const register = async (userData: any) => { // Função de registro do usuário
+    const response = await axios.post(API_URL + "register", userData);
+    if (response.data.token) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+    }
+    return response.data;
+};
+
+const login = async (userData: any) => { // Função de login do usuário
+    const response = await axios.post(API_URL + "login", userData);
+    if (response.data.token) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+    }
+    return response.data;
+};
