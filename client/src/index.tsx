9959cfc4e1a82ae30b1d1a377f328df2fc54e2a3
Annanda <165848254+AnnandaV@users.noreply.github.com>
2025-11-03 06:42:39 -0300

    ajuste nas dependências

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    {/* 3. Envolva o App com o Provider */}
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root") as HTMLElement | null;
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      {/* 3. Envolva o App com o Provider */}
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  );
+}
